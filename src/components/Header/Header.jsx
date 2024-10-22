e945337c6cd84c2d12b3acc4af08c94facfdd043
Arman <mdarman365506@gmail.com>
2024-10-22 11:21:31 +0600

    added blogs and bookmark section only

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 
+import profile from '../../assets/images/profile.png'
 
 const Header = () => {
     return (
-      <div>
-        <h1 className="text-6xl">Knowledge Cafe</h1>
-      </div>
+      <header className='flex justify-between items-center p-4 mx-4 border-b-2'>
+            <h1 className="text-3xl">Knowledge Cafe</h1>
+            <img src={profile} alt="" />
+      </header>
     );
 };
 
